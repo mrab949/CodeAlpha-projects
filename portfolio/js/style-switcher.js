90885fde3cffda12380a733a75ab15c2ac2e4aad
Muhammad Rizwan <157191553+mrab949@users.noreply.github.com>
2025-02-05 00:16:49 +0500

    Update style-switcher.js

--- portfolio/js/style-switcher.js
+++ portfolio/js/style-switcher.js
@@ -4,14 +4,6 @@ styleSwitcherToggle.addEventListener('click', ()=> {
     document.querySelector('.style-switcher').classList.toggle('open');
 });
 
-//hide style switcher on scroll 
-
-// window.addEventListener('scroll', ()=>{
-//     if(document.querySelector('.style-switcher').classList.contains('open')){
-//         document.querySelector('.style-switcher').classList.remove('open');
-//     }
-// });
-
 document.querySelector('.section').addEventListener("click", ()=>{
     if(document.querySelector('.style-switcher').classList.contains('open')){
         document.querySelector('.style-switcher').classList.remove('open');
